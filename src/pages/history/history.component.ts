4b75a6c11e3e42e4f508ab77d18c97d099864a13
Zilo <zilanbal@yahoo.fr>
2026-03-17 13:13:34 +0100

    FEAT: Ajout de la gestion des dates et des lettres trouvées dans l'historique des parties

--- src/pages/history/history.component.ts
+++ src/pages/history/history.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { GameCatalog } from '../../features/services/game-catalog';
 import { RouterLink } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-history',
-  imports: [RouterLink],
+  imports: [RouterLink, DatePipe],
   templateUrl: './history.page.html',
 })
 export class HistoryPage {
